697e4534d2effbceb1c210dbcac13485bcb2de8b
Jason Bell <jbell@accelebrate.com>
2025-08-13 18:57:59 -0700

    Playground app pt 1

---
+++ Playground/Playground/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Playground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Please provide a number: ");
+                string input = Console.ReadLine() ?? "0";
+                int num;
+                bool isNumber = int.TryParse(input, out num);
+                if (!isNumber)
+                {
+                    Console.WriteLine("BAD NUMBER!");
+                    continue;
+                }
+                else
+                {
+                    if (num == 0) break;
+                    nums.Add(num);
+                }
+            }
+
+            var evens = nums.ToArray();
+
+            foreach (int even in evens)
+            {
+                Console.WriteLine(even);
+            }
+        }
+    }
+}
