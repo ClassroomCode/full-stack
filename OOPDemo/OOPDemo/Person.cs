d0d407906a165dcd0c59c2ebddefc937b76b5def
Jason Bell <jbell@accelebrate.com>
2025-08-18 14:46:28 -0700

    OOP part 1

---
+++ OOPDemo/OOPDemo/Person.cs
@@ -0,0 +1,17 @@
+﻿
+namespace OOPDemo
+{
+    internal class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        /*
+         * 1. Properties should not throw exceptions (try/catch)
+         * 2. Properties should not possibly take a long time to execute (thread)
+         * 3. Properties should result in same end state no matter how many times called
+         */
+    }
+}
---
+++ OOPDemo/OOPDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace OOPDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+
+            p.FirstName = "Bill";
+            p.LastName = "Gates";
+
+            Console.WriteLine(p.FullName);
+        }
+    }
+}
