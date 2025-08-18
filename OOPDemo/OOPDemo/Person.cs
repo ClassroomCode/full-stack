2d3b7840b18fd20ae6cbd43889d78f4ef8755a9e
Jason Bell <jbell@accelebrate.com>
2025-08-18 15:07:25 -0700

    oop part 2

--- OOPDemo/OOPDemo/Person.cs
+++ OOPDemo/OOPDemo/Person.cs
@@ -6,12 +6,19 @@ namespace OOPDemo
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
 
-        public string FullName => $"{FirstName} {LastName}";
+        public virtual string FullName => $"{FirstName} {LastName}";
 
         /*
          * 1. Properties should not throw exceptions (try/catch)
          * 2. Properties should not possibly take a long time to execute (thread)
          * 3. Properties should result in same end state no matter how many times called
          */
+    }
+
+    internal class Employee : Person
+    {
+        public double Salary { get; set; }
+
+        public override string FullName => $"{LastName}, {FirstName}";
     }
 }
--- OOPDemo/OOPDemo/Program.cs
+++ OOPDemo/OOPDemo/Program.cs
@@ -4,12 +4,30 @@
     {
         static void Main(string[] args)
         {
-            var p = new Person();
+            var people = new List<Person>();
 
+            var p = new Person();
             p.FirstName = "Bill";
             p.LastName = "Gates";
+            people.Add(p);
 
-            Console.WriteLine(p.FullName);
+            var emp = new Employee();
+            emp.FirstName = "Steve";
+            emp.LastName = "Jobs";
+            emp.Salary = 100000;
+            people.Add(emp);
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                Person person = people[i];
+                Console.WriteLine(person.FullName);
+            }
+            /*
+            foreach (Person x in people)
+            {
+                Console.WriteLine(x.FullName);
+            }
+            */
         }
     }
 }
