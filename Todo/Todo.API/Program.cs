29ec9f324a16ea0c092905d28752cc8bd840e35a
Jason Bell <jbell@accelebrate.com>
2025-08-20 14:51:47 -0700

    With some Todos

--- Todo/Todo.API/Program.cs
+++ Todo/Todo.API/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Todo.API;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TodoDb>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
-// TODO: do real stuff
+using (var serviceScope = app.Services.CreateScope())
+{
+    var services = serviceScope.ServiceProvider;
+    var db = services.GetRequiredService<TodoDb>();
+    db.Todos.Add(new TodoItem { Id = 1, Name = "One", IsComplete = false });
+    db.Todos.Add(new TodoItem { Id = 2, Name = "Two", IsComplete = true });
+    await db.SaveChangesAsync();
+}
 
 app.Run();
