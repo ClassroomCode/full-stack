1e813722cdbb3169e0d75f4c9c19154ea7868ea9
Jason Bell <jbell@accelebrate.com>
2025-09-05 12:05:30 -0700

    Changes to TodoDb

--- Todo/Todo.DataAccess/TodoDb.cs
+++ Todo/Todo.DataAccess/TodoDb.cs
@@ -3,12 +3,39 @@ using Todo.Entities;
 
 namespace Todo.DataAccess;
 
-public class TodoDb : DbContext
+internal class TodoDb : DbContext, ITodoDb
 {
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TodoList");
+    }
 
     public DbSet<TodoItem> Todos { get; set; }
+
+    public Task AddTodo(TodoItem todo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteTodo(TodoItem todo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TodoItem>> GetAllTodos()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TodoItem?> GetTodo(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateTodo(TodoItem todo)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
