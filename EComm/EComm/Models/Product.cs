ed7d7858103aaf36d07aa3c8059605d198a88638
Jason Bell <jbell@accelebrate.com>
2025-09-08 13:43:44 -0700

    Validation in Product

--- EComm/EComm/Models/Product.cs
+++ EComm/EComm/Models/Product.cs
@@ -1,7 +1,22 @@
-﻿namespace EComm.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Product
+namespace EComm.Models;
+
+public class Product : IValidatableObject
 {
     public int ProductID { get; set; }
+
+    [MinLength(3, ErrorMessage ="Name too short")]
     public string ProductName { get; set; } = "";
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var retVal = new List<ValidationResult>();
+
+        if (ProductName.StartsWith("x"))
+        {
+            retVal.Add(new ValidationResult("Name can't start with x", ["name"]));
+        }
+        return retVal;
+    }
 }
